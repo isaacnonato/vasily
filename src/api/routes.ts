a7a9520de1e911c488c15cb2c683ba9ba613970b
Isaac Nonato <isaac.sart@protonmail.com>
2022-05-07 17:15:05 -0300

    removed duplicated method

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -32,26 +32,4 @@ module.exports = (app: Application, prisma: PrismaClient): void => {
         });
       });
   });
-
-  app.get('/:id', async (req, res) => {
-    const url = await prisma.url.findUnique({
-      where: {
-        redirect_id: req.params.id,
-      }
-    });
-
-    if (url == null) {
-      res.json({
-        status: "error",
-        message: "The requested URL does not exist."
-      }).status(500);
-      return;
-    }
-    let redirectUrl: string = url.long_url;
-    if (redirectUrl.slice(0, 8) != "https://") {
-      redirectUrl = 'https://' + redirectUrl
-    } 
-
-    res.redirect(redirectUrl);
-  });
 };
